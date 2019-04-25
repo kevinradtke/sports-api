4029495b058253218084b99448b7b6e3bb1dc9ea
Kevin Radtke <kevinradtkeg@gmail.com>
2019-04-25 09:34:48 -0500

    added to public files

--- public/script.js
+++ public/script.js
@@ -11,9 +11,9 @@ function displaySportList(data){
 }
 
 function onload(){
-	let url = "./sports/api/list-sports";
+	let url = './sports/api/list-sports';
 	let settings = {
-		method : "GET",
+		method : 'GET',
 		headers : {
 			'Content-Type' : 'application/json'
 		}
@@ -31,8 +31,65 @@ function onload(){
 		})
 		.catch(err => {
 			console.log(err);
-		})
+		});
 }
 
+function updateSportList(data){
+	$('.sports-list').append(`<li>
+								${data.sport.name}
+							  </li>`);
+}
 
-$(onload);
+function addNewSport(name, id){
+
+	let data = {
+		name : name,
+		id : id
+	};
+
+	let url = './sports/api/post-sport';
+	let settings = {
+						method : 'POST',
+						headers : {
+							'Content-Type' : 'application/json'
+						},
+						body : JSON.stringify(data)
+					};
+
+	fetch(url, settings)
+		.then(response => {
+			if (response.ok){
+				return response.json();
+			}
+			else{
+				return new Promise(function(resolve, reject){
+					resolve(response.json());
+				})
+				.then(data =>{
+					throw new Error(data.message);
+				})
+			}
+		})
+		.then(responseJSON => {
+			updateSportList(responseJSON);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+}
+
+function watchForm(){
+	$('.sportForm').on('submit', function(event) {
+		event.preventDefault();
+		let name = $('.sportName').val();
+		let id = $('.sportId').val();
+		addNewSport(name, id);
+	});
+}
+
+function init(){
+	$(onload);
+	$(watchForm);
+}
+
+$(init);
